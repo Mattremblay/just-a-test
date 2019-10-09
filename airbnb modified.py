7462693528b14105965897dcc9f6def434c35c0a
Mattremblay <56325918+Mattremblay@users.noreply.github.com>
2019-10-08 20:26:33 -0400

    Create airbnb modified.py

---
+++ airbnb modified.py
@@ -0,0 +1,11 @@
+import sys
+filename = sys.argv[1]
+min = sys.argv[2]
+max = sys.argv[3]
+f = open(filename)
+lines = f.readlines()
+for line in lines[1:]:
+    columns = line.split(',')
+    price = int (columns[9])
+    if price >= int(min) and price <= int(max):
+        print (line)
